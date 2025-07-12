83a57b9e25365b22a819df126aae038140a6f6d4
Ryan Kim <ryankim@Ryans-Laptop.lan>
2025-07-11 21:05:43 -0700

    trigger dev deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./database/database.module";
 import { PullModule } from "./domains/pull/pull.module";
+import { HealthCheckModule } from "./domains/health-check/health-check.module";
 
 @Module({
-  imports: [DatabaseModule, PullModule],
+  imports: [DatabaseModule, PullModule, HealthCheckModule],
 })
 export class AppModule {}
