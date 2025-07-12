6fdee64429149561d52887765be07d0d7a7ddf39
Ryan Kim <ryankim@Ryans-Laptop.lan>
2025-07-11 22:46:29 -0700

    open up CORS to frontend apps

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "https://tcg-world-ui.vercel.app",
+      "http://localhost:3000",
+      "https://tcg-world-ui-env-dev-tcg-world-d1b60ad9.vercel.app",
+    ],
   });
   await app.listen(3001);
 }
